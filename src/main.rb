fcb8a4df2df5bd57d545980a6a809f0d59f19123
robert <0x1eef@hardenedbsd.org>
2026-06-17 21:52:39 +0000

    main: forward -p to the program being execed

--- src/main.rb
+++ src/main.rb
@@ -2,13 +2,19 @@ def main(argv)
   command = []
   pledges = ["exec", "proc"]
   has_pledges = false
+  has_command = false
   while o = argv.shift
     case o
     when "-p"
-      pledges.push(argv.shift)
-      has_pledges = true
+      if has_command
+        command.push(o)
+      else
+        pledges.push(argv.shift)
+        has_pledges = true
+      end
     else
       if has_pledges
+        has_command = true
         command.push(o)
       else
         warn "pledge: -p switch must appear before command"
