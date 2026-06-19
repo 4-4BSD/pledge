442a3329399444ed379692db7b8bbd11d8c23359
0x1eef <0x1eef@hardenedbsd.org>
2026-06-19 12:12:12 -0300

    main: refactor, document and fix

--- src/main.rb
+++ src/main.rb
@@ -1,13 +1,57 @@
+##
+# main
+
 def main(argv)
+  ##
+  # The following pledges are required to bootstrap
+  # a process. The new process inherits these abilities,
+  # plus whatever other pledges are provided at the command
+  # line.
+  pledges = ["exec", "proc", "rpath", "stdio"]
   command = []
-  pledges = ["exec", "proc"]
-  has_pledges = false
+  if argv.empty?
+    return help
+  end
+  parse_options(argv, pledges, command)
+  pledge(pledges.join(" "))
+  execvp(*command)
+end
+
+##
+# Utils
+
+def help
+  warn <<~'HELP'
+  pledge [OPTIONS] program arguments
+
+  Options:
+    -p    The name of a pledge.
+    -h    Show help.
+
+  Examples:
+    pledge -p ioctl ls /
+    pledge -p inet -p ioctl curl https://freebsd.org
+  HELP
+end
+
+def parse_options(argv, pledges, command)
   has_command = false
-  while o = argv.shift
-    case o
+  has_pledges = false
+  while option = argv.shift
+    case option
+    when "-h"
+      if has_command
+        command.push(option)
+      elsif has_pledges
+        warn "pledge: -h and -p can't be combined\n"
+        exit(1)
+      else
+        help
+        exit(0)
+      end
     when "-p"
       if has_command
-        command.push(o)
+        command.push(option)
       else
         pledges.push(argv.shift)
         has_pledges = true
@@ -15,14 +59,16 @@ def main(argv)
     else
       if has_pledges
         has_command = true
-        command.push(o)
+        command.push(option)
       else
-        warn "pledge: -p switch must appear before command"
+        warn "pledge: -p switch must appear before command\n"
         exit(1)
       end
     end
   end
-  pledge(pledges.join(" "))
-  execvp(*command)
 end
+
+##
+# Let's go
+
 main(ARGV)
